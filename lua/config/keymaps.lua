8c5cb607bcaf60925e139ef84d1ee5590ec9ff85
Austin Chappell <austin.w.chappell@gmail.com>
2026-07-21 23:54:06 -0500

    Move literal-grep and yank-path keymaps under <leader>a

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,10 +8,18 @@ vim.keymap.set("i", "<C-g>u", function()
 end, { desc = "Insert UUID" })
 
 -- Search for exact literal strings, completely bypassing regex
-vim.keymap.set("n", "<leader>sF", function()
+vim.keymap.set("n", "<leader>af", function()
   require("telescope.builtin").live_grep({
     additional_args = function()
       return { "--fixed-strings" }
     end,
   })
 end, { desc = "Grep literal string (Exact Match)" })
+
+-- Yank the current buffer's path, relative to the cwd
+vim.keymap.set("n", "<leader>ap", function()
+  local path = vim.fn.expand("%:.")
+  vim.fn.setreg('"', path)
+  vim.fn.setreg("+", path)
+  vim.notify("Yanked path: " .. path)
+end, { desc = "Yank relative path" })
