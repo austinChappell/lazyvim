73c879c2fc49dd33293c95c11470fb6a12d7da16
Austin Chappell <austin.w.chappell@gmail.com>
2026-07-21 23:39:03 -0500

    Add Telescope with fzf-native and literal-string grep keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,12 @@ vim.keymap.set("i", "<C-g>u", function()
   local uuid = vim.fn.system("uuidgen"):gsub("\n", ""):lower()
   vim.api.nvim_put({ uuid }, "c", true, true)
 end, { desc = "Insert UUID" })
+
+-- Search for exact literal strings, completely bypassing regex
+vim.keymap.set("n", "<leader>sF", function()
+  require("telescope.builtin").live_grep({
+    additional_args = function()
+      return { "--fixed-strings" }
+    end,
+  })
+end, { desc = "Grep literal string (Exact Match)" })
